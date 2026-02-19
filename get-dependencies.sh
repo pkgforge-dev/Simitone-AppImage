76b888ee3eec9bed9cb6791d0bcfc777820cbfe8
Link4Electronics <link4electronics@gmail.com>
2026-02-19 04:36:54 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,7 +17,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package mono-msbuild-git
 
 # If the application needs to be manually built that has to be done down here
 echo "Making nightly build of Simitone..."
