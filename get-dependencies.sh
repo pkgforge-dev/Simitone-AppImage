9a8da060d6f56f42faa3a09eb305ac429555e918
Link4Electronics <link4electronics@gmail.com>
2026-02-19 14:08:08 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -34,21 +34,19 @@ echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 cd ./Simitone
+find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <PackageReference Include="System.Drawing.Common" Version="9.0.0" />' {} +
+find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <GenerateRuntimeConfigurationFiles>true</GenerateRuntimeConfigurationFiles>\n    <RuntimeHostConfigurationOption Include="System.Drawing.EnableUnixSupport" Value="true" />' {} +
 find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <EnableWindowsTargeting>true</EnableWindowsTargeting>' {} +
 find . -name "*.csproj" -exec sed -i 's/net[0-9].0-windows/net10.0/g' {} +
 find . -name "*.csproj" -exec sed -i 's/Microsoft.NET.Sdk.WindowsDesktop/Microsoft.NET.Sdk/g' {} +
 find . -name "*.csproj" -exec sed -i 's/<UseWindowsForms>true<\/UseWindowsForms>/<UseWindowsForms>false<\/UseWindowsForms>/g' {} +
 find . -name "*.csproj" -exec sed -i 's/<UseWPF>true<\/UseWPF>/<UseWPF>false<\/UseWPF>/g' {} +
-find . -name "*.csproj" -exec sed -i 's/Microsoft.NET.Sdk.WindowsDesktop/Microsoft.NET.Sdk/g' {} +
 find . -name "*.csproj" -exec sed -i 's/<OutputType>WinExe<\/OutputType>/<OutputType>Library<\/OutputType>/g' {} +
-#find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <PackageReference Include="System.Drawing.Common" Version="9.0.0" />\n    <GenerateRuntimeConfigurationFiles>true</GenerateRuntimeConfigurationFiles>' {} +
-#find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <RuntimeHostConfigurationOption Include="System.Drawing.EnableUnixSupport" Value="true" />' {} +
-#find . -name "*.csproj" -exec sed -i 's/Microsoft.NET.Sdk.WindowsDesktop/Microsoft.NET.Sdk/g' {} +
-#find . -name "*.csproj" -exec sed -i 's/<OutputType>WinExe<\/OutputType>/<OutputType>Library<\/OutputType>/g' {} +
 cd Client/Simitone
 sed -i '/FSO.IDE/d' Simitone.sln
 sed -i '/FSO.Windows/d' Simitone.sln
 sed -i '/Simitone.Windows/d' Simitone.sln
+
 dotnet restore Simitone.sln
 dotnet build Simitone.sln -c Release --no-restore
 mv -v bin/Release/* ../../../AppDir/bin
