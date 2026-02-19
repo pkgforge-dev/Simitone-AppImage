52aac6b8b55da3d304001fc71a07e14ddd0ee6e5
Link4Electronics <link4electronics@gmail.com>
2026-02-19 04:26:15 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,12 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    libdecor     \
+    mono         \
+    mono-msbuild \
+    openal       \
+    sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -16,6 +21,17 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of Simitone..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/riperiperi/Simitone"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./Simitone
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./Simitone/Client/Simitone
+msbuild Simitone.sln /p:Configuration=Release
+mv -v bin/Release/Simitone.Windows.exe ../../../AppDir/bin
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
