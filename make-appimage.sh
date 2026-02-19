4f2db1ef56e0bc0c041e2126986446cfadce51c0
Link4Electronics <link4electronics@gmail.com>
2026-02-19 16:42:28 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,6 +11,7 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/Simitone ./AppDir/bin/lib/Simitone
+echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 
