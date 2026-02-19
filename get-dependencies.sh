d7c0ed8367d9f50da0cb38ead5ca1b07016b949c
Link4Electronics <link4electronics@gmail.com>
2026-02-19 13:25:40 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -26,23 +26,25 @@ make-aur-package dotnet-core-bin
 # If the application needs to be manually built that has to be done down here
 echo "Making nightly build of Simitone..."
 echo "---------------------------------------------------------------"
-REPO="https://github.com/riperiperi/Simitone"
+#REPO="https://github.com/riperiperi/Simitone"
+REPO="https://github.com/alexjyong/Simitone"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
 git clone --recursive --depth 1 "$REPO" ./Simitone
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 cd ./Simitone
-find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <PackageReference Include="System.Drawing.Common" Version="9.0.0" />\n    <GenerateRuntimeConfigurationFiles>true</GenerateRuntimeConfigurationFiles>' {} +
-find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <RuntimeHostConfigurationOption Include="System.Drawing.EnableUnixSupport" Value="true" />' {} +
-find . -name "*.csproj" -exec sed -i 's/Microsoft.NET.Sdk.WindowsDesktop/Microsoft.NET.Sdk/g' {} +
-find . -name "*.csproj" -exec sed -i 's/<OutputType>WinExe<\/OutputType>/<OutputType>Library<\/OutputType>/g' {} +
+#find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <PackageReference Include="System.Drawing.Common" Version="9.0.0" />\n    <GenerateRuntimeConfigurationFiles>true</GenerateRuntimeConfigurationFiles>' {} +
+#find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <RuntimeHostConfigurationOption Include="System.Drawing.EnableUnixSupport" Value="true" />' {} +
+#find . -name "*.csproj" -exec sed -i 's/Microsoft.NET.Sdk.WindowsDesktop/Microsoft.NET.Sdk/g' {} +
+#find . -name "*.csproj" -exec sed -i 's/<OutputType>WinExe<\/OutputType>/<OutputType>Library<\/OutputType>/g' {} +
 cd Client/Simitone
-sed -i '/FSO.IDE/d' Simitone.sln
-sed -i '/Simitone.Windows/d' Simitone.sln
-sed -i '/FSO.Windows/d' Simitone.sln
-dotnet restore Simitone.sln
-dotnet build Simitone.sln -c Release --no-restore
+#sed -i '/FSO.IDE/d' Simitone.sln
+#sed -i '/Simitone.Windows/d' Simitone.sln
+#sed -i '/FSO.Windows/d' Simitone.sln
+#dotnet restore Simitone.sln
+#dotnet build Simitone.sln -c Release --no-restore
+dotnet build Simitone.sln -c Release
 mv -v bin/Release/* ../../../AppDir/bin
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
