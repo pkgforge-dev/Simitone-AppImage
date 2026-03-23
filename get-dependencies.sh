5e0c55c405306fb094e91f82c1bc38929a8a4c66
Link4Electronics <link4electronics@gmail.com>
2026-03-23 10:24:23 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,57 +7,30 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    libdecor    \
-    libgdiplus  \
-    xmlstarlet  \
-    openal      \
-    sdl2        \
-    unzip
-    #mono        \
+    dotnet-runtime-9.0  \
+    libgdiplus          \
+    xmlstarlet          \
+    openal              \
+    pipewire-audio      \
+    pipewire-jack       \
+    sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package dotnet-core-bin
-#make-aur-package dotnet-core-9.0-bin
-#make-aur-package mono-msbuild-git
-
-# If the application needs to be manually built that has to be done down here
-if [ "$ARCH" = "x86_64" ]; then
-    VERSION=0.8.20-forked
-    echo "$VERSION" > ~/version
-    wget https://github.com/alexjyong/Simitone/releases/download/v$VERSION/Simitone-Linux-x64-Release.zip
-
-    mkdir -p ./AppDir/bin
-    bsdtar -xvf Simitone-Linux-x64-Release.zip -C ./AppDir/bin
-    rm -rf ./AppDir/bin/simitone.desktop
-else
-    echo "Making nightly build of Simitone..."
-    echo "---------------------------------------------------------------"
-    #REPO="https://github.com/riperiperi/Simitone"
-    REPO="https://github.com/alexjyong/Simitone"
-    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-    git clone --recursive --depth 1 "$REPO" ./Simitone
-    echo "$VERSION" > ~/version
-
-    mkdir -p ./AppDir/bin
-    cd ./Simitone
-    find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <PackageReference Include="System.Drawing.Common" Version="9.0.0" />' {} +
-    find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <GenerateRuntimeConfigurationFiles>true</GenerateRuntimeConfigurationFiles>\n    <RuntimeHostConfigurationOption Include="System.Drawing.EnableUnixSupport" Value="true" />' {} +
-    find . -name "*.csproj" -exec sed -i '/<\/PropertyGroup>/i \    <EnableWindowsTargeting>true</EnableWindowsTargeting>' {} +
-    find . -name "*.csproj" -exec sed -i 's/net[0-9].0-windows/net10.0/g' {} +
-    find . -name "*.csproj" -exec sed -i 's/Microsoft.NET.Sdk.WindowsDesktop/Microsoft.NET.Sdk/g' {} +
-    find . -name "*.csproj" -exec sed -i 's/<UseWindowsForms>true<\/UseWindowsForms>/<UseWindowsForms>false<\/UseWindowsForms>/g' {} +
-    find . -name "*.csproj" -exec sed -i 's/<UseWPF>true<\/UseWPF>/<UseWPF>false<\/UseWPF>/g' {} +
-    find . -name "*.csproj" -exec sed -i 's/<OutputType>WinExe<\/OutputType>/<OutputType>Library<\/OutputType>/g' {} +
-    cd Client/Simitone
-    sed -i '/FSO.IDE/d' Simitone.sln
-    sed -i '/FSO.Windows/d' Simitone.sln
-    sed -i '/Simitone.Windows/d' Simitone.sln
-
-    dotnet restore Simitone.sln
-    dotnet build Simitone.sln -c Release --no-restore
-    mv -v bin/Release/* ../../../AppDir/bin
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+ZIP_LINK=$(wget https://api.github.com/repos/alexjyong/Simitone/releases -O - \
+      | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*Linux-x64-Release.zip")
+echo "$ZIP_LINK" | awk -F'/' '{tag=$(NF-1); gsub(/^v/, "", tag); print tag; exit}' > ~/version
+if ! wget --retry-connrefused --tries=30 "$ZIP_LINK" -O /tmp/app.zip 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
 fi
+
+mkdir -p ./AppDir/bin/lib/Content/MeshReplace
+bsdtar -xvf /tmp/app.zip -C ./AppDir/bin
+mv -v ./AppDir/bin/Simitone ./AppDir/bin/Simlauncher
+rm -f ./AppDir/bin/simitone.desktop
+rm -f ./AppDir/bin/lib/*.pdb
