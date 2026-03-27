2327a50ff5e4e6b499b90439f719c061bfdf5201
Link4Electronics <link4electronics@gmail.com>
2026-03-26 22:20:15 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -25,16 +25,13 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package dotnet-core-9.0-bin
 
 # If the application needs to be manually built that has to be done down here
-wget https://github.com/Link4Electronics/Simitone/releases/download/v0.8.21-forked/Simitone-Linux-x64-Release.zip -O /tmp/app.zip
-VERSION=0.8.21-forked
-echo "$VERSION" > ~/version
-#ZIP_LINK=$(wget https://api.github.com/repos/alexjyong/Simitone/releases -O - \
-#      | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*Linux-x64-Release.zip")
-#echo "$ZIP_LINK" | awk -F'/' '{tag=$(NF-1); gsub(/^v/, "", tag); print tag; exit}' > ~/version
-#if ! wget --retry-connrefused --tries=30 "$ZIP_LINK" -O /tmp/app.zip 2>/tmp/download.log; then
-#	cat /tmp/download.log
-#	exit 1
-#fi
+ZIP_LINK=$(wget https://api.github.com/repos/alexjyong/Simitone/releases -O - \
+      | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*Linux-x64-Release.zip")
+echo "$ZIP_LINK" | awk -F'/' '{tag=$(NF-1); gsub(/^v/, "", tag); print tag; exit}' > ~/version
+if ! wget --retry-connrefused --tries=30 "$ZIP_LINK" -O /tmp/app.zip 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
 
 mkdir -p ./AppDir/bin
 bsdtar -xvf /tmp/app.zip -C ./AppDir/bin
