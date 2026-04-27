fb50064e3c83b2d4ec1eb718782dc74ccedf5b08
Link4Electronics <link4electronics@gmail.com>
2026-04-27 11:52:38 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,8 +12,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-#quick-sharun ./AppDir/bin/Simlauncher ./AppDir/bin/lib/Simitone
-quick-sharun ./AppDir/bin/Simlauncher ./AppDir/bin/lib
+quick-sharun ./AppDir/bin/Simlauncher ./AppDir/bin/lib/Simitone
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 # this app has problems with other locales breaking physics
 echo 'LC_ALL=C.UTF-8' >> ./AppDir/.env
