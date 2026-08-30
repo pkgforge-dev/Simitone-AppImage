7d16371451d30250f7a448256041a0141f7590cc
Link4Electronics <link4electronics@gmail.com>
2026-08-30 05:18:21 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -20,3 +20,7 @@ ln -s /tmp/simitone_debug.log ./AppDir/bin/lib/simitone_debug.log
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
