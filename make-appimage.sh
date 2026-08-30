6cbfcace8597750a0a5a1e47fc726b661d6b8b60
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:46:43 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,7 +12,7 @@ export USE_HOST_DRIVERS_EXPERIMENTAL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/Simlauncher ./AppDir/bin/lib/Simitone
+quick-sharun ./AppDir/bin/Simlauncher ./AppDir/bin/lib/Simitone /usr/lib/libopenal.so*
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # attempts to write to a read only location
